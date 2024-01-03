959e536f711a6ea4e2626b94ffcda2f5d96b1386
Martin Pradny <pradny@pris.to>
2024-01-03 21:48:07 +0100

    Removed sample code

--- main.ts
+++ main.ts
@@ -1,21 +1,8 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
+import { Notice, Plugin, TFile, TFolder } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class PromoteNotePlugin extends Plugin {
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
+	async onload() {		
 		this.registerEvent(
 			this.app.workspace.on("file-menu", (menu, file) => {
 				if (file instanceof TFile && file.extension == "md") {
@@ -28,79 +15,10 @@ export default class MyPlugin extends Plugin {
 				}
 			})
 		)
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 
 	async promoteNote(file: TFile) {
@@ -112,7 +30,7 @@ export default class MyPlugin extends Plugin {
 		// Extract the base name without the .md extension
 		const baseName = file.basename;
 
-		var newFolderPath: string;
+		let newFolderPath: string;
 		// Construct the path for the new folder
 		if (file.parent) {
 			newFolderPath = file.parent.path + '/' + baseName;
@@ -126,7 +44,7 @@ export default class MyPlugin extends Plugin {
 		// Construct the new path for the file
 		const newFilePath = newFolderPath + '/' + file.name;
 
-		var attachmentsFolderPath = "";
+		let attachmentsFolderPath = "";
 		if (hasEmbeds) {
 			// we must get the attachment folder before moving
 			attachmentsFolderPath = await this.getAvailablePathForAttachments("", "", file);
@@ -136,19 +54,19 @@ export default class MyPlugin extends Plugin {
 		await this.app.fileManager.renameFile(file, newFilePath);
 
 		if (hasEmbeds) {
-			const newFile = this.app.vault.getAbstractFileByPath(newFilePath);			
+			const newFile = this.app.vault.getAbstractFileByPath(newFilePath);
 			if (newFile instanceof TFile) {
 				await this.moveLinkedFiles(file, attachmentsFolderPath, newFile);
 			}
 			new Notice(`${newFilePath}  created and images moved`);
-debugger;
+
 			if (await this.deleteIfEmpty(attachmentsFolderPath)) {
 				new Notice(`${attachmentsFolderPath}  folder deleted`);
 			}
 		} else {
 			new Notice(`${newFilePath}  created`);
 		}
-		
+
 	}
 
 	async moveLinkedFiles(oldFile: TFile, attachmentsFolderPath: string, newFile: TFile) {
@@ -177,15 +95,15 @@ debugger;
 	async deleteIfEmpty(folderPath: string): Promise<boolean> {
 		// Get the folder object
 		if (folderPath.endsWith('/')) {
-			folderPath = folderPath.slice(0, -1); 
+			folderPath = folderPath.slice(0, -1);
 		}
 		const folder = this.app.vault.getAbstractFileByPath(folderPath);
-	
+
 		// Make sure it's a folder and not a file
 		if (folder instanceof TFolder) {
 			// Get the contents of the folder
 			const contents = folder.children;
-	
+
 			// If the folder is empty (no files or subfolders)
 			if (contents.length === 0) {
 				// Delete the folder
@@ -198,13 +116,13 @@ debugger;
 	}
 
 	/**
- * This is a helper method for an undocumented API of Obsidian.
- *
- * @param fileName The Filename for your Attachment
- * @param format The Fileformat of your Attachment
- * @param sourceFile The Sourcefile from where the Attachment gets added, this is needed because the Attachment Folder might be different based on where it gets inserted.
- * @returns The Attachment Path
- */
+	 * This is a helper method for an undocumented API of Obsidian.
+	 *
+	 * @param fileName The Filename for your Attachment
+	 * @param format The Fileformat of your Attachment
+	 * @param sourceFile The Sourcefile from where the Attachment gets added, this is needed because the Attachment Folder might be different based on where it gets inserted.
+	 * @returns The Attachment Path
+	 */
 	async getAvailablePathForAttachments(
 		fileName: string,
 		format: string,
@@ -216,44 +134,4 @@ debugger;
 
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
